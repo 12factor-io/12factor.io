4fa03046f98f697bfece956e930016d60d255e7b
Edward Wilde <ewilde@gmail.com>
2017-10-12 19:01:59 +0100

    middleman init

--- website/config.rb
+++ website/config.rb
@@ -6,7 +6,6 @@ activate :autoprefixer do |prefix|
 end
 
 activate :livereload
-
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
@@ -42,7 +41,7 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-configure :build do
-  activate :minify_css
-  activate :minify_javascript
-end
+# configure :build do
+#   activate :minify_css
+#   activate :minify_javascript
+# end
