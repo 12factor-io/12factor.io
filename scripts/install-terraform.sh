1a8afe4f95dce2c7f90719f3e0b4dbe974e08ee3
Edward Wilde <ewilde@gmail.com>
2017-11-15 07:43:36 +0000

    Travis debugging - before tmp

--- scripts/install-terraform.sh
+++ scripts/install-terraform.sh
@@ -4,7 +4,9 @@ TERRAFORM_VERSION=0.10.8
 OS=linux
 PROCESSOR=amd64
 
+pushd /tmp
 wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_${PROCESSOR}.zip -O terraform.zip
-sudo unzip -p terraform.zip terraform >/usr/local/bin/terraform
-sudo chmod +x /usr/local/bin/terraform
+unzip terraform.zip
+mv ./terraform /usr/local/bin/
 rm terraform.zip
+popd
